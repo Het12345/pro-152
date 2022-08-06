60b0391e58fc2bc6e981884026c8b54cb674dfb2
Het12345 <90767991+Het12345@users.noreply.github.com>
2022-08-06 10:35:58 +0530

    Add files via upload

---
+++ move.js
@@ -0,0 +1,51 @@
+// Registering component in box-component.js
+AFRAME.registerComponent("move-car", {
+    schema: {
+      moveY: { type: "number", default: 0 },
+    },
+  
+    tick: function () {
+      window.addEventListener("click",(e)=>{
+        this.data.moveY = this.data.moveY + 0.01;
+      })
+  
+      var pos = this.el.getAttribute("position");
+  
+      pos.y = this.data.moveY;
+  
+      this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+    }
+  });
+  AFRAME.registerComponent("zoom-out", {
+    schema:{
+        moveZ: {type:"number", default:10},
+    },
+    tick:function(){
+        this.data.moveZ = this.data.moveZ + 0.01;
+        var pos = this.el.getAttribute("position");
+  
+        pos.z = this.data.moveZ;
+    
+        this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+    }
+  });
+
+
+  // Registering component in box-component.js
+AFRAME.registerComponent("fall-down", {
+    schema: {
+      moveY: { type: "number", default: 0 },
+    },
+  
+    tick: function () {
+      window.addEventListener("click",(e)=>{
+        this.data.moveY = this.data.moveY - 0.001;
+      })
+  
+      var pos = this.el.getAttribute("position");
+  
+      pos.y = this.data.moveY;
+  
+      this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+    }
+  });
